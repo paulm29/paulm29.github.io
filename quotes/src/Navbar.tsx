c13e56f6072629e769da49171e0ad7b6531aa001
paulm29 <paul.miscellaneous@gmail.com>
2023-07-01 04:46:55 +1000

    styles

--- quotes/src/Navbar.tsx
+++ quotes/src/Navbar.tsx
@@ -4,6 +4,7 @@ const Navbar = () => {
     return (
         <div>
             <NavLink to="/">Home</NavLink>
+            &nbsp;
             <NavLink to="/about">About</NavLink>
         </div>
     )
